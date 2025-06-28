cf95018a82d6dd986127564297dadc09253c49f9
Mohabbat <mohabbatalit8@gmail.com>
2025-06-28 23:38:44 +0600

    update build file error

--- src/Components/Common/FetchUrls.jsx
+++ src/Components/Common/FetchUrls.jsx
@@ -1,3 +1,3 @@
-// export const FetchUrls = (url) =>
-//   `https://hotelorion-backend.vercel.app/api/${url}`;
-export const FetchUrls = (url) => `http://localhost:5000/api/${url}`;
+export const FetchUrls = (url) =>
+  `https://hotel-management-backend-v1.vercel.app/api/${url}`;
+// export const FetchUrls = (url) => `http://localhost:5000/api/${url}`;
--- src/redux/baseApi/baseApi.js
+++ src/redux/baseApi/baseApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://hotelorion-backend.vercel.app/api",
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://hotel-management-backend-v1.vercel.app/",
+    // baseUrl: "http://localhost:5000/api",
   }),
 
   tagTypes: [
